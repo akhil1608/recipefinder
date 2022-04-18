912e46a45cb6f7e5cb296ff19893787285971719
akhil <akhil160896@outlook.com>
2022-04-18 17:09:45 -0400

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
 	$("#loginform").on("submit", function() {
-		const formData = new FormData(event.target);
-	  	const formDataJSON = {};
-	  	formData.forEach((value, key) => (formDataJSON[key] = value));
-		data = "'"+JSON.stringify(formDataJSON)+"'";
-		console.log(data);
+		formData = "{\"username\": \"" + $("#username").val() + "\", \"password\": \"" + $("#password").val() + "\"}";
 		$.ajax("https://85ox413pdj.execute-api.us-east-1.amazonaws.com/authenticate", {
 			type: "POST",
-			data: data,
+			data: formData
 		})
 		.done(function (data, textStatus, jqXHR) {
 		    	console.log(data);
