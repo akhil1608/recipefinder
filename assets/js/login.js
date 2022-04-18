6b4bd80bce866b8aba4632036c24db74c7a85a1b
akhil <akhil160896@outlook.com>
2022-04-18 18:43:30 -0400

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	$("#loginform").on("submit", function() {
 		formData = { "username": $("#username").val(), "password": $("#password").val() };
-		console.log(formData);
 		$.ajax("https://85ox413pdj.execute-api.us-east-1.amazonaws.com/authenticate", {
 			type: "GET",
 			data: formData,
@@ -11,13 +10,13 @@ $(document).ready(function() {
 			if (data.Count == 1)
 				$(location).attr("href", "https://google.com");
 			else {
-				$("errormsg").text("Invalid username/password.");
-				$("errormsg").show();
+				$("#errormsg").text("Invalid username/password.");
+				$("#errormsg").show();
 			}
 		})
 		.fail(function (jqXHR, textStatus, errorThrown) {
-			$("errormsg").text("Server error. Try again later.");
-			$("errormsg").show();
+			$("#errormsg").text("Server error. Try again later.");
+			$("#errormsg").show();
 		});
 		event.preventDefault();
 	});
