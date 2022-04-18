28a248a80938939e80b8df7af5001b432be49d96
akhil <akhil160896@outlook.com>
2022-04-18 19:07:32 -0400

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$("#loginform").on("submit", function() {
+		$("#errormsg").hide();
 		formData = { "username": $("#username").val(), "password": $("#password").val() };
 		$.ajax("https://85ox413pdj.execute-api.us-east-1.amazonaws.com/authenticate", {
 			type: "GET",
@@ -7,8 +8,10 @@ $(document).ready(function() {
 			dataType: "json"
 		})
 		.done(function (data, textStatus, jqXHR) {
-			if (data.Count == 1)
-				$(location).attr("href", "https://google.com");
+			if (data.Count == 1) {
+				document.cookie = "username=" + $("#username").val() + "; path=/";
+				$(location).attr("href", "https://akhil1608.github.io/recipefinder");
+			}
 			else {
 				$("#errormsg").text("Invalid username/password.");
 				$("#errormsg").show();
